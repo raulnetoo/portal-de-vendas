9ca8a312b51ddcd1d9284484a541b6a9fefc57e6
raulnetoo <142536710+raulnetoo@users.noreply.github.com>
2025-11-26 15:16:25 -0300

    att

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/portal-de-vendas/', // <<-- substitua pelo nome do repo no GitHub
   plugins: [react()],
-})
+});
